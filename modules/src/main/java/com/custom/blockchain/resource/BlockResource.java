074f11a7e943643efa198ce2347cd0572e889aba
Marcos Rachid <marcosrachid@gmail.com>
2018-03-03 19:18:21 -0300

    miner profile does not have endpoints

--- modules/src/main/java/com/custom/blockchain/resource/BlockResource.java
+++ modules/src/main/java/com/custom/blockchain/resource/BlockResource.java
@@ -1,5 +1,6 @@
 package com.custom.blockchain.resource;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.custom.blockchain.handler.BlockHandler;
@@ -9,6 +10,7 @@ import com.custom.blockchain.handler.BlockHandler;
  * @author marcosrachid
  *
  */
+@Profile("!miner")
 @RestController
 public class BlockResource {
 	
--- modules/src/main/java/com/custom/blockchain/resource/WalletResource.java
+++ modules/src/main/java/com/custom/blockchain/resource/WalletResource.java
@@ -9,6 +9,7 @@ import javax.websocket.server.PathParam;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ import com.custom.blockchain.resource.dto.response.ResponseDTO;
  * @author marcosrachid
  *
  */
+@Profile("!miner")
 @RestController
 @RequestMapping(value = "/wallet")
 public class WalletResource {
